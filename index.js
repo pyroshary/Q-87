398decdc2d9e0e7470adedb54cf3f69cbbcd6a59
pyroshary <sheharyarghulamrasool@gmail.com>
2024-04-06 04:02:40 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+function extractcode(str) {
+    return str.substring(0, 10);
+}
+console.log(extractcode("Hey there i am trying to extract the code "));
---
+++ index.ts
@@ -0,0 +1,5 @@
+function extractcode (str:string): string{
+    return str.substring(0,10)
+}
+
+console.log(extractcode("Hey there i am trying to extract the code "))
